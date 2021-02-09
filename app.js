11d8b528fe0204b0547951136f5a281224b8207f
che000 <atuikhar@gmail.com>
2021-02-09 18:17:52 +0530

    ok

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+  res.render('home');
+});
+
+app.listen(3000, function (req, res) {
+  console.log('Server started...');
+});
